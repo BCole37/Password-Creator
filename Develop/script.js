afd1af4dd4d8a29ea663141c54b700df6948aac3
Bry Cole <Brysonc98@live.com>
2021-08-21 21:11:45 -0700

    first commit

---
+++ Develop/script.js
@@ -0,0 +1,127 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var numIncluded;
+var specialCharacterIncluded;
+var uppercaseIncluded;
+var lowercaseIncluded;
+var charactersSelected = [];
+
+
+// setting up the different charcter sets then spliting them to an array
+var nums = '0123456789'.split(
+  ''  
+  );
+var specialCharacters = '!"#$%&()*+,-./:;<=>?@[]^_`{|}~'.split(
+  ''  
+  );
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(
+  ''  
+  );
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split(
+''  
+);
+
+
+// Write password to the #password input
+function writePassword() {
+  function generatePassword(){
+    // making the user input the amount of characters the password will be
+    numAmount = prompt("How many Characters? Between 8 and 128");
+    // checking that something was input
+    if(!numAmount){
+      alert("Requieres numeric value")
+      // if the number is low or high then telling user to try again
+    }else if (numAmount < 8 || numAmount > 128) {
+       numAmount = prompt("Must be between 8 and 128");
+    } else {
+      // once an acceptable number is presented checks which list the user wants to pick from
+      numIncluded = confirm("Will this contain numbers?");
+      specialCharacterIncluded = confirm("Will this contain special characters?");
+      uppercaseIncluded = confirm("Will this contain Uppercase letters?");
+      lowercaseIncluded = confirm("Will this contain Lowercase letters?");
+  };
+
+  
+  // tells user a character list must be selected  
+  if(!numIncluded && !specialCharacterIncluded && !uppercaseIncluded && !lowercaseIncluded){
+   choiceError = alert("You must choose a character type");
+  }
+
+  // adds a selected list to selected characters based upon user input
+  if(numIncluded == true && specialCharacterIncluded == true && uppercaseIncluded == true && lowercaseIncluded == true){
+    charactersSelected = charactersSelected.concat(nums, specialCharacters ,upperCase ,lowerCase)
+    console.log(charactersSelected,"allincluded")
+  } else if (numIncluded == true && specialCharacterIncluded == true && uppercaseIncluded == true && lowercaseIncluded == false){
+    charactersSelected = charactersSelected.concat(nums, specialCharacters ,upperCase)
+    console.log(charactersSelected,"nolowercase")
+  } else if (numIncluded == true && specialCharacterIncluded == true && uppercaseIncluded == false && lowercaseIncluded == true){
+    charactersSelected = charactersSelected.concat(nums, specialCharacters ,lowerCase)
+    console.log(charactersSelected,"noUpper")
+  } else if (numIncluded == true && specialCharacterIncluded == false && uppercaseIncluded == true && lowercaseIncluded == true){
+    charactersSelected = charactersSelected.concat(nums, lowerCase ,upperCase)
+    console.log(charactersSelected,"nospec")
+  } else if (numIncluded == false && specialCharacterIncluded == true && uppercaseIncluded == true && lowercaseIncluded == true){
+    charactersSelected = charactersSelected.concat(lowerCase, specialCharacters ,upperCase)
+    console.log(charactersSelected,"nonums")
+  } else if (numIncluded == false && specialCharacterIncluded == false && uppercaseIncluded == true && lowercaseIncluded == true){
+    charactersSelected = charactersSelected.concat(lowerCase, upperCase)
+    console.log(charactersSelected,"nonums or spec")
+  } else if (numIncluded == false && specialCharacterIncluded == true && uppercaseIncluded == false && lowercaseIncluded == true){
+    charactersSelected = charactersSelected.concat(specialCharacters ,lowerCase)
+    console.log(charactersSelected,"nonums or upper")
+  } else if (numIncluded == false && specialCharacterIncluded == true && uppercaseIncluded == true && lowercaseIncluded == false){
+    charactersSelected = charactersSelected.concat(specialCharacters ,upperCase)
+    console.log(charactersSelected,"nonums or lower")
+  } else if (numIncluded == true && specialCharacterIncluded == false && uppercaseIncluded == false && lowercaseIncluded == true){
+    charactersSelected = charactersSelected.concat(nums ,lowerCase)
+    console.log(charactersSelected,"no spec or upper")
+  } else if (numIncluded == true && specialCharacterIncluded == false && uppercaseIncluded == true && lowercaseIncluded == false){
+    charactersSelected = charactersSelected.concat(nums ,upperCase)
+    console.log(charactersSelected,"no spec or lower")
+  } else if (numIncluded == true && specialCharacterIncluded == true && uppercaseIncluded == false && lowercaseIncluded == false){
+    charactersSelected = charactersSelected.concat(nums ,specialCharacters)
+    console.log(charactersSelected,"no upper or lower")
+  } else if (numIncluded == true && specialCharacterIncluded == false && uppercaseIncluded == false && lowercaseIncluded == false){
+    charactersSelected = charactersSelected.concat(nums )
+    console.log(charactersSelected,"nums")
+  } else if (numIncluded == false && specialCharacterIncluded == true && uppercaseIncluded == false && lowercaseIncluded == false){
+    charactersSelected = charactersSelected.concat(specialCharacters )
+    console.log(charactersSelected,"specialCharacters")
+  } else if (numIncluded == false && specialCharacterIncluded == false && uppercaseIncluded == true && lowercaseIncluded == false){
+    charactersSelected = charactersSelected.concat(upperCase )
+    console.log(charactersSelected,"upperCase")
+  } else if (numIncluded == false && specialCharacterIncluded == false && uppercaseIncluded == false && lowercaseIncluded == true){
+    charactersSelected = charactersSelected.concat(lowerCase )
+    console.log(charactersSelected,"lowerCase")
+  }
+
+
+
+  // sets up an empty string
+  var password = [];
+
+  // Start random selection variables:
+  // Random selection for all variables: 
+  for (var i = 0; i < numAmount; i++) {
+      var passwordCharacters = charactersSelected[Math.floor(Math.random() * charactersSelected.length)];
+      password.push(passwordCharacters);
+  }
+  // This joins the password array and converts it to a string
+  
+  var truePassword = password.join("");
+
+
+  return truePassword;
+  }
+
+
+  var password = generatePassword()
+  var passwordText = document.querySelector("#password");
+
+
+  passwordText.value = password;
+  console.log(password.value)
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
---
+++ README.md
@@ -0,0 +1,36 @@
+# Password Creator 
+
+## Project Detials
+This Project was to use the javascript we learned to create a password genorator and a interactive page.
+
+# Why
+This demonstrates that there is more levels of polish and interaction that can be implimented onto a webpage allowing for more creativity and input from the user.
+This Password Creator serves a direct purpose for the user allowing them to input specifications and then receive something off of their own specifications.
+
+# Problems
+I didn't run into many problems. I was stuck for a time on trying to figure out how to see where things got hung up and once a TA reminded me i could console log
+out information it made debugging much easier to see where things were.
+The homework for this week helped solve almost all of my issues directly.
+
+
+# Problems Solved
+-Generated prompts for what characters are included 
+-corrects if incorrect input for amopunt of characters so must be between 8 and 128
+-the character set is responsive to user inputs only choosing from what they select.
+-displays generated password once complete.
+
+
+# Takaways
+I need to understandthe math functions and prompt types better
+I am more comfertable with java than i thought i would be
+I want to figure out how to do the selections without so many if else statements 
+
+# Sources For Help
+ Bootcamp spot Module three ("https://coding-bootcamp-fsf-prework.readthedocs-hosted.com/en/latest/modules/chapter3/")
+ w3schools("https://www.w3schools.com/jsref/jsref_join.asp")
+
+# ScreenShot
+(./images/Week3ScreenShot.png)
+
+# Link
+[Link to the website](https://bcole37.github.io/Password-Creator/)
